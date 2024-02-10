a6a3aca71513b4237afac8023cfa14f7d0c95140
Abdu <abdu@Mac.local>
2024-02-10 11:54:48 +0530

    push changes upto localhost

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51M5tawSC7cBO6OVNe4DzodZV7h2w4qYUbiRTg8xzp23C0aOwdvM2CUP7ZGLm3SnX8ahDkWWWalKck2cmwwB8Pc2N009eHXq8Wm');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
